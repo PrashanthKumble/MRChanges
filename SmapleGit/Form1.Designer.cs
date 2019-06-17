cbe70a975044b4132b9bbfd162cce73d252204e6
PrashanthKumble <36465730+PrashanthKumble@users.noreply.github.com>
2019-06-17 18:04:22 -0400

    First Set of file added

---
+++ SmapleGit/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace SmapleGit
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_save = new System.Windows.Forms.Button();
+            this.txt_name = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btn_save
+            // 
+            this.btn_save.Location = new System.Drawing.Point(62, 124);
+            this.btn_save.Name = "btn_save";
+            this.btn_save.Size = new System.Drawing.Size(247, 45);
+            this.btn_save.TabIndex = 0;
+            this.btn_save.Text = "Save";
+            this.btn_save.UseVisualStyleBackColor = true;
+            this.btn_save.Click += new System.EventHandler(this.btn_save_Click);
+            // 
+            // txt_name
+            // 
+            this.txt_name.Location = new System.Drawing.Point(62, 54);
+            this.txt_name.Name = "txt_name";
+            this.txt_name.Size = new System.Drawing.Size(242, 20);
+            this.txt_name.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(883, 353);
+            this.Controls.Add(this.txt_name);
+            this.Controls.Add(this.btn_save);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_save;
+        private System.Windows.Forms.TextBox txt_name;
+    }
+}
+
